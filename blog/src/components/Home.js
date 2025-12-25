71b0c7c343f1b9e0906ad3edc6268f92f909c6c3
Ishan Mahajan <im530@cam.ac.uk>
2025-12-25 19:38:24 +0000

    Lession5: Click Events

--- blog/src/components/Home.js
+++ blog/src/components/Home.js
@@ -1,7 +1,22 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Hello World", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("Hello " + name, e);
+  };
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button
+        onClick={(e) => {
+          handleClickAgain("Ishan", e);
+        }}
+      >
+        Click me again
+      </button>
     </div>
   );
 };
