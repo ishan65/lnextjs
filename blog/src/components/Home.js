9534c94b317e997008fdb6741a52e337d829b6f9
Ishan Mahajan <im530@cam.ac.uk>
2025-12-25 23:34:52 +0000

    Lession8: use of map fuction

--- blog/src/components/Home.js
+++ blog/src/components/Home.js
@@ -2,21 +2,36 @@ import { useState } from "react";
 
 const Home = () => {
   // let name = "mario";
-  const [name, setName] = useState("mario");
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    setName("ishan");
-    setAge(37);
-  };
+  const [blogs, setBlogs] = useState([
+    {
+      id: 1,
+      title: "My website",
+      body: "It is a good website.",
+      author: "Ishan",
+    },
+    {
+      id: 2,
+      title: "your website",
+      body: "It is a better website.",
+      author: "Nidhi",
+    },
+    {
+      id: 3,
+      title: "Our website",
+      body: "It is a best website.",
+      author: "Neelam",
+    },
+  ]);
 
   return (
     <div className="home">
       <h2>Home Page</h2>
-      <p>
-        {name} is {age} old
-      </p>
-      <button onClick={handleClick}>Click Me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>blog.title</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
