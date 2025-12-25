6129de05d48945eb0610ea1c93f54b9c4cd1e623
Ishan Mahajan <im530@cam.ac.uk>
2025-12-25 18:20:49 +0000

    Lesson 2: Use variables in App.js

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 
 function App() {
+  const title = "Welcome to new blog";
+  const likes = 50;
+  // const person = { name: "yoshi", age: 30 };   // bool and object in component will give error
+  const link = "https://www.google.com";
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>liked {likes} times</p>
+        {/* <p>{person}</p> */}
+        <p>{10}</p>
+        <p>{"Hello Ishan"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 10}</p>
+        <a href={link}>Google</a>
       </div>
     </div>
   );
