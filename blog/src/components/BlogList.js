c51806ae6b0d6b48eaf81f71f305bbad53675300
Ishan Mahajan <im530@cam.ac.uk>
2025-12-26 18:54:01 +0000

    fetching data with useEffect

--- blog/src/components/BlogList.js
+++ blog/src/components/BlogList.js
@@ -8,7 +8,8 @@ const BlogList = (props) => {
       <h1>{title}</h1>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>blog.title</h2>
+          <h2>{blog.title}</h2>
+          <p>{blog.body}</p>
           <p>Written by {blog.author}</p>
           <button
             onClick={() => {
--- blog/src/components/Home.js
+++ blog/src/components/Home.js
@@ -3,28 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   // let name = "mario";
-  const [blogs, setBlogs] = useState([
-    {
-      id: 1,
-      title: "My website",
-      body: "It is a good website.",
-      author: "Ishan",
-    },
-    {
-      id: 2,
-      title: "your website",
-      body: "It is a better website.",
-      author: "Nidhi",
-    },
-    {
-      id: 3,
-      title: "Our website",
-      body: "It is a best website.",
-      author: "Neelam",
-    },
-  ]);
-
-  const [name, setName] = useState("mario");
+  const [blogs, setBlogs] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => {
@@ -34,23 +13,22 @@ const Home = () => {
   };
 
   useEffect(() => {
-    console.log("use effect ran");
-    console.log(blogs);
-  }, [name]);
+    fetch("http://localhost:5789/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
       <h2>Home Page</h2>
-      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
-      <BlogList
-        blogs={blogs.filter((blog) => {
-          return blog.author === "Neelam";
-        })}
-        title="Neelam's blogs"
-        handleDelete={handleDelete}
-      />
-      <button onClick={() => setName("ishan")}>Change name</button>
-      <p>{name}</p>
+      {blogs && (
+        <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      )}
     </div>
   );
 };
