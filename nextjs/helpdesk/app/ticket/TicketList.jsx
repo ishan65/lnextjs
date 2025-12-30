ffcde668ef0df41b0e721b8f7d1948c50d6e44e9
Ishan Mahajan <im530@cam.ac.uk>
2025-12-30 19:09:56 +0000

    Enable Dynamic Segments params

--- nextjs/helpdesk/app/ticket/TicketList.jsx
+++ nextjs/helpdesk/app/ticket/TicketList.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 async function getTickets() {
   const res = await fetch("http://localhost:4000/tickets", {
     next: {
@@ -13,11 +14,13 @@ export const TicketList = async () => {
     <>
       {ticket.map((ticket) => (
         <div key={ticket.id} className="card my-5">
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
-          </div>
+          <Link href={`/ticket/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
         </div>
       ))}
       {ticket.length == 0 && (
