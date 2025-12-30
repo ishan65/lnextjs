bae34da85622893e514b9fafdb9490351c1e0057
Ishan Mahajan <im530@cam.ac.uk>
2025-12-30 20:18:44 +0000

    Loading UI and suspense

--- nextjs/helpdesk/app/ticket/page.jsx
+++ nextjs/helpdesk/app/ticket/page.jsx
@@ -1,4 +1,6 @@
+import { Suspense } from "react";
 import { TicketList } from "./TicketList";
+import Loading from "../loading";
 
 const tickets = () => {
   return (
@@ -11,7 +13,9 @@ const tickets = () => {
           </p>
         </div>
       </nav>
-      <TicketList />
+      <Suspense fallback={<Loading />}>
+        <TicketList />
+      </Suspense>
     </main>
   );
 };
